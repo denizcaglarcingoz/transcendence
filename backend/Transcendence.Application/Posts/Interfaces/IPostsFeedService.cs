441043f4bc6fbe32a7518d12bb8cd4e5b1b6dd9e
denizcaglarcingoz <denizcaglarcingoz@gmail.com>
2026-02-10 21:53:29 +0100

    The final update before holiday. PostsFeedController and PostsProfileController are not done yet. Their equiavalent of service, dto, interfaces are also not done. The rest is finished.

---
+++ backend/Transcendence.Application/Posts/Interfaces/IPostsFeedService.cs
@@ -0,0 +1,8 @@
+﻿using Transcendence.Application.Posts.DTOs;
+
+namespace Transcendence.Application.Posts.Interfaces;
+public interface IPostsFeedService
+{
+	Task<CursorPageDto<FeedPostDto>> GetFeedAsync(int take, string? cursor, Guid currentUserId, CancellationToken ct);
+}
+
